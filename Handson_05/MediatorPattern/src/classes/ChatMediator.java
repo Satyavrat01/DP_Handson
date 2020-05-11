83a2951c19042ae3b607ab0146199ac69f89a755
Satyavrat01 <64369217+Satyavrat01@users.noreply.github.com>
2020-05-11 09:11:28 +0530

    Add files via upload

---
+++ Handson_05/MediatorPattern/src/classes/ChatMediator.java
@@ -0,0 +1,35 @@
+package classes;
+
+import java.util.List;
+
+import interfaces.IChatMediator;
+import interfaces.IUser;
+
+public class ChatMediator implements IChatMediator {
+	
+	private List<IUser> users;
+	public ChatMediator() {}
+
+	@Override
+	public void addUser(IUser user) {
+			users.add(user);
+	}
+
+	public List<IUser> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<IUser> users) {
+		this.users = users;
+	}
+	
+	@Override
+	public void sendMessage(String msg) {
+		for(IUser user:users)
+		{
+			user.recieveMessage(msg);
+		}
+		
+	}
+
+}
---
+++ Handson_05/MediatorPattern/src/interfaces/IChatMediator.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface IChatMediator {
+	void addUser(IUser user);
+
+	void sendMessage(String msg);
+}
